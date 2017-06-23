19c0175313bfc8636f34efab37f60d35fb563666
yanhuasiyu <20984557@qq.com>
2017-06-23 22:25:53 +0800

    wechat sever ok:

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat;
+
 class WechatController extends Controller
 {
-    public function wechat()
+    public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        $server = EasyWeChat::server();
 
-        $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
-        });
-
-        Log::info('return response.');
-
-        return $wechat->server->serve();
+        return $server->serve();
     }
 }
