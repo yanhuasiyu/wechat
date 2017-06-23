6584f2e7ba57d59f91da94ea66d73f7f615e4cc5
yanhuasiyu <20984557@qq.com>
2017-06-23 20:59:49 +0800

    git hook test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel git hook?
                 </div>
 
                 <div class="links">
