83a6772cb3699cf65ee8da792dac90c50a2e25f7
yanhuasiyu <20984557@qq.com>
2017-06-23 22:02:56 +0800

    Laravel Easy WeChat 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Easy WeChat
+                    Laravel Easy WeChat 1
                 </div>
 
                 <div class="links">
