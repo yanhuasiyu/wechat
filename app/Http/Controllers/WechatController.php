88beb5713b3ae6c9d03d719efbca30671fcc0886
yanhuasiyu <20984557@qq.com>
2017-06-23 22:42:07 +0800

    æmessage

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -9,7 +9,37 @@ class WechatController extends Controller
     public function serve()
     {
         $server = EasyWeChat::server();
-
+        $server->setMessageHandler(function ($message) {
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+            // ...
+        });
+        
         return $server->serve();
     }
 }
