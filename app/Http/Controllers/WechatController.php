b6260ea34db4f4abe4cccdb90a99e386690dee13
yanhuasiyu <20984557@qq.com>
2017-06-23 21:56:53 +0800

    serve

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,42 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use EasyWeChat\Foundation\Application;
-use EasyWeChat;
+
 
 class WechatController extends Controller
 {
     public function wechat()
     {
-        $server = EasyWeChat::server();
-        $server->setMessageHandler(function($message){
-            switch ($message->MsgType) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 overtrue！";
         });
-        return $server->serve();
+
+        Log::info('return response.');
+
+        return $wechat->server->serve();
     }
 }
