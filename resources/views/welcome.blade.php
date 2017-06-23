c5eb63c772746d3c4ce9bccd89b99fc94dab44ea
yanhuasiyu <20984557@qq.com>
2017-06-23 21:11:44 +0800

    install wasy wechat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel git hook?
+                    Laravel Easy WeChat
                 </div>
 
                 <div class="links">
