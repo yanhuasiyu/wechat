e42ef3582992c78ab994d3a0128000a118d0c306
yanhuasiyu <20984557@qq.com>
2017-06-23 21:31:17 +0800

    add wechatController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,12 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Easy WeChat
+                    Laravel Easy WeChat 1
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://easywechat.org/" target="_black">Easy Wechat</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
