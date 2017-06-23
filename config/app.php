d3091757656fdbbcf6a7794738040238be3932b1
yanhuasiyu <20984557@qq.com>
2017-06-23 21:11:44 +0800

    install wasy wechat

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
 
         Orphans\GitDeploy\GitDeployServiceProvider::class,
+        Overtrue\LaravelWechat\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'EasyWeChat' => Overtrue\LaravelWechat\Facade::class,
 
     ],
 
