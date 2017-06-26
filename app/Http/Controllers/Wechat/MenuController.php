19d5df53e7fc29c1a2d397badaa73a25f75146ca
yanhuasiyu <20984557@qq.com>
2017-06-26 22:22:26 +0800

    menu all

--- app/Http/Controllers/Wechat/MenuController.php
+++ app/Http/Controllers/Wechat/MenuController.php
@@ -14,6 +14,7 @@ class MenuController extends Controller
     {
         $this->menu = EasyWeChat::menu();
     }
+
     /**
      * Display a listing of the resource.
      *
@@ -21,7 +22,7 @@ class MenuController extends Controller
      */
     public function index()
     {
-        $menus = $menus->all();
+        $menus = $this->$menu->all();
         return $menus;
     }
 
@@ -38,7 +39,7 @@ class MenuController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -49,7 +50,7 @@ class MenuController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -60,7 +61,7 @@ class MenuController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -71,8 +72,8 @@ class MenuController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -83,7 +84,7 @@ class MenuController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
