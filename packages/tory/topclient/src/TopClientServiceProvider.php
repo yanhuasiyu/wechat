e1d4a161997681c2d5157b163572305530b64034
yanhuasiyu <20984557@qq.com>
2017-06-27 11:13:40 +0800

    add tbk

---
+++ packages/tory/topclient/src/TopClientServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tory\TopClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class TopClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/topclient.php' => config_path('topclient.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
