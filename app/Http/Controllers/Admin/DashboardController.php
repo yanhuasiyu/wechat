d97c2e596965f284dd407229092ade8c5b7772cf
yanhuasiyu <20984557@qq.com>
2017-06-28 09:38:47 +0800

    admin login

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth.admin:admin');
+    }
+
+    
+    public function index()
+    {
+        dd('后台首页，当前用户名：'.auth('admin')->user()->name);
+    }
+}
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            $url = $guard ? 'admin/dash':'/';
+            return redirect($url);
         }
 
         return $next($request);
