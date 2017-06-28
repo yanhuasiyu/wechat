83fbb3177517a50030be52612472e36c6ada13b9
yanhuasiyu <20984557@qq.com>
2017-06-28 22:03:34 +0800

    'now'

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,11 +9,8 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-        Debugbar::addMessage('index', 'wechat 2');
-=======
         \Debugbar::addMessage('index', 'wechat 2');
->>>>>>> e84f1e2... return nickname
+
         return view('welcome');
     }
 }
